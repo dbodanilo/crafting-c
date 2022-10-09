5d9df6d924f4c5e3889ed1b51b59a86f29eab6eb
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-10-09 19:13:29 -0300

    add chapter 25: Closures

--- clox/object.c
+++ clox/object.c
@@ -13,16 +13,31 @@
 static Obj* allocateObject(size_t size, ObjType type) {
   Obj* object = (Obj*)reallocate(NULL, 0, size);
   object->type = type;
-  
+
   object->next = vm.objects;
-  vm.objects = object;  
+  vm.objects = object;
 
   return object;
+}
+
+ObjClosure* newClosure(ObjFunction* function) {
+  ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
+                                   function->upvalueCount);
+  for (int i = 0; i < function->upvalueCount; i++) {
+    upvalues[i] = NULL;
+  }
+
+  ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+  closure->function = function;
+  closure->upvalues = upvalues;
+  closure->upvalueCount = function->upvalueCount;
+  return closure;
 }
 
 ObjFunction* newFunction() {
   ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
+  function->upvalueCount = 0;
   function->name = NULL;
   initChunk(&function->chunk);
 
@@ -35,7 +50,7 @@ ObjNative* newNative(NativeFn function) {
   return native;
 }
 
-static ObjString* allocateString(char* chars, int length, 
+static ObjString* allocateString(char* chars, int length,
                                  uint32_t hash) {
   ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
   string->length = length;
@@ -57,7 +72,7 @@ static uint32_t hashString(const char* key, int length) {
 
 ObjString* takeString(char* chars, int length) {
   uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindString(&vm.strings, chars, length, 
+  ObjString* interned = tableFindString(&vm.strings, chars, length,
                                         hash);
   if (interned != NULL) {
     FREE_ARRAY(char, chars, length + 1);
@@ -69,8 +84,8 @@ ObjString* takeString(char* chars, int length) {
 
 ObjString* copyString(const char* chars, int length) {
   uint32_t hash = hashString(chars, length);
-  ObjString* interned = tableFindString(&vm.strings, chars, length, 
-                                        hash);  
+  ObjString* interned = tableFindString(&vm.strings, chars, length,
+                                        hash);
   if (interned != NULL) return interned;
 
   char* heapChars = ALLOCATE(char, length + 1);
@@ -78,6 +93,14 @@ ObjString* copyString(const char* chars, int length) {
   heapChars[length] = '\0';
 
   return allocateString(heapChars, length, hash);
+}
+
+ObjUpvalue* newUpvalue(Value* slot) {
+  ObjUpvalue* upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+  upvalue->closed = NIL_VAL;
+  upvalue->location = slot;
+  upvalue->next = NULL;
+  return upvalue;
 }
 
 static void printFunction(ObjFunction* function) {
@@ -90,6 +113,9 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+    case OBJ_CLOSURE:
+      printFunction(AS_CLOSURE(value)->function);
+      break;
     case OBJ_FUNCTION:
       printFunction(AS_FUNCTION(value));
       break;
@@ -98,6 +124,9 @@ void printObject(Value value) {
       break;
     case OBJ_STRING:
       printf("%s", AS_CSTRING(value));
+      break;
+    case OBJ_UPVALUE:
+      printf("upvalue");
       break;
   }
 }
