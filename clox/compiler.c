221843e5f83b519a25e7d7695d13c18b09fb9235
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-10-27 09:48:27 -0300

    add up to section 28.2 Method References

--- clox/compiler.c
+++ clox/compiler.c
@@ -258,6 +258,8 @@ static void defineVariable(uint8_t global);
 static uint8_t parseVariable(const char* errorMessage);
 
 static uint8_t identifierConstant(Token* name);
+static void namedVariable(Token name, bool canAssign);
+
 static int resolveLocal(Compiler* compiler, Token* name);
 static int resolveUpvalue(Compiler* compiler, Token* name);
 static void markInitialized();
@@ -353,16 +355,32 @@ static void function(FunctionType type) {
   }
 }
 
+static void method() {
+  consume(TOKEN_IDENTIFIER, "Expect method name.");
+  uint8_t constant = identifierConstant(&parser.previous);
+
+  FunctionType type = TYPE_FUNCTION;
+  function(type);
+
+  emitBytes(OP_METHOD, constant);
+}
+
 static void classDeclaration() {
   consume(TOKEN_IDENTIFIER, "Expect class name.");
+  Token className = parser.previous;
   uint8_t nameConstant = identifierConstant(&parser.previous);
   declareVariable();
 
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
 
+  namedVariable(className, false);
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    method();
+  }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
+  emitByte(OP_POP);
 }
 
 static void funDeclaration() {
